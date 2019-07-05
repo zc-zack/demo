e24ab8a71323e84266cb2459111dafb6405a46a7
沙漏的凝滞 <1301842136@qq.com>
2019-07-05 14:32:10 +0800

    添加扫描

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -1,7 +1,13 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AdminController {
+
+    @RequestMapping("/admin")
+    public String admin(){
+        return "admin/admin";
+    }
 }
